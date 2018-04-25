189a3f3935819ef198536ab324ccb383d98c983a
Art Dahm <art@dahm.com>
2018-04-25 15:37:13 -0400

    Fix typo

--- Feather_Matrix_8x16_BLE_Message.ino
+++ Feather_Matrix_8x16_BLE_Message.ino
@@ -5,7 +5,7 @@ https://github.com/adafruit/Adafruit_BluefruitLE_nRF51/tree/master/examples/bleu
 
 The MIT License (MIT)
 
-Copyright 2018 Arthur J. ahm III
+Copyright 2018 Arthur J. Dahm III
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 documentation files (the "Software"), to deal in the Software without restriction, including without limitation
